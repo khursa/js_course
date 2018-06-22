a92cbbb3a17e08eecb1239a5318d45564e65f24c
Khursa <khursa@protonmail.com>
2018-06-22 21:59:41 +0300

    add part of module 3

---
+++ module 3/js/index.js
@@ -0,0 +1,7 @@
+import * as UIController from "ui_controller.js";
+
+UIController.prototype.run = function changeInput() {
+    focus.addEventListener('keydown', function() {
+        let pattern = /[A-Za-z]{2,}/;
+      });
+};
