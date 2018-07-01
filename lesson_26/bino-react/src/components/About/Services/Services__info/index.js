8fda1cd037438cc6c0e2fb1e958aac6dc2a82919
Khursa <khursa@protonmail.com>
2018-07-01 21:46:07 +0300

    add block 'Services' to bino-react

--- lesson_26/bino-react/src/components/About/Services/Services__info/index.js
+++ lesson_26/bino-react/src/components/About/Services/Services__info/index.js
@@ -7,13 +7,13 @@ class Services__info extends Component {
       <div className="services__info">
         <Services__title services__title="OUR SERVICES"/>
         <Services__subtitle__web  subtitle__web ="WEB DESIGN"/>
-        <Services__text services__text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+        <Services__text1 services__text__1="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
         Lorem Ip sum has been the industry's standard dummy text ever."/>
         <Services__subtitle__print  subtitle__print ="PRINT DESIGN"/>
-        <Services__text services__text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+        <Services__text2 services__text__2="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
         Lorem Ip sum has been the industry's standard dummy text ever."/>
         <Services__subtitle__photo  subtitle__photo ="PHOTOGRAPHY"/>
-        <Services__text services__text="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
+        <Services__text3 services__text__3="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  
         Lorem Ip sum has been the industry's standard dummy text ever."/>
       </div>
     );
@@ -58,10 +58,26 @@ const Services__subtitle__photo = (props) => {
   )
 }
 
-const Services__text = (props) => {
+const Services__text1 = (props) => {
   return (
-     <p className="services__text">
-        { props.services__text }     
+     <p className="services__text--1">
+        { props.services__text__1 }     
+    </p>
+  )
+}
+
+const Services__text2 = (props) => {
+  return (
+     <p className="services__text--2">
+        { props.services__text__2 }     
+    </p>
+  )
+}
+
+const Services__text3 = (props) => {
+  return (
+     <p className="services__text--3">
+        { props.services__text__3 }     
     </p>
   )
 }
